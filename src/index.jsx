29eabb1a66376385104b693000c41fc803e6bab9
Cedric Franz <cedric.franz@gmail.com>
2020-08-13 11:52:24 +0200

    Initial version

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => <div>Hello React!</div>;
+
+ReactDOM.render(<Index />, document.getElementById('index'));
+
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+ 
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', "eslint-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    htmlPlugin,
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
